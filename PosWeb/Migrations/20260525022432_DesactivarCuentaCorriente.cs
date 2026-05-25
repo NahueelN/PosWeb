3186cf27ac8594086a2071dca71fe2deb513c6fc
NahueelN <nahuel.25.45@gmail.com>
2026-05-24 23:25:11 -0300

    fix: deactivate Cuenta Corriente payment method

---
+++ PosWeb/Migrations/20260525022432_DesactivarCuentaCorriente.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PosWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class DesactivarCuentaCorriente : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE MEDIOS_PAGO SET ACTIVO = 0 WHERE ID_MEDIO_PAGO = 5;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE MEDIOS_PAGO SET ACTIVO = 1 WHERE ID_MEDIO_PAGO = 5;");
+        }
+    }
+}
